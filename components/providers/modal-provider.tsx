931f8b6c324ae25951d550daebdd6b46cecbfd47
macsimoum <a.r.hassannezhad@gmail.com>
2023-09-02 19:05:14 +0330

    Fix #1

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,11 +2,11 @@
 
 import { useEffect, useState } from "react";
 
-import { DeleteMessageModal } from "../modals/delete-message-modal";
 import { MessageFileModal } from "../modals/message-file-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
+import { DeleteMessageModal } from "@/components/modals/delete-message-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { EditChannelModal } from "@/components/modals/edit-channel-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
