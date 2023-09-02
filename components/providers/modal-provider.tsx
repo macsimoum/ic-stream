c3af539bd8298d2067030a3adc80ccc605e7c069
macsimoum <a.r.hassannezhad@gmail.com>
2023-09-02 19:56:06 +0330

    Fix #2

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from "react";
 
-import { MessageFileModal } from "../modals/message-file-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
@@ -13,6 +12,7 @@ import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
+import { MessageFileModal } from "@/components/modals/message-file-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
